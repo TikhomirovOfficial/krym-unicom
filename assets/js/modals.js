99cbd9d20b67512e853286d8092e16461c713d1a
Artem <arteom.tixomirow@yandex.ru>
2022-07-02 20:18:21 +0300

    added new pages and adapted

--- assets/js/modals.js
+++ assets/js/modals.js
@@ -6,16 +6,17 @@ menuButton.click(() => {
     menuButton.toggleClass('present-menu__opened')
 })
 
-const sertificateEl = $('.sertificate-img')
+const sertificateEl = $('.sertificate-item')
 const modalSertificate = $('.modal-image')
 const modalSertificateImage = $('.modal-sertificate__img')
 const modalSertificateClose = $('.modal-close-btn')
 
 sertificateEl.click(function() {
-    const srcImage = $(this).attr('src')
+    const srcImage = $($(this).children('img')[0]).attr('src')
     modalSertificateImage.attr('src', srcImage)
     modalSertificate.toggleClass('modal-image__opened')
 })
 modalSertificateClose.click(() => {
     modalSertificate.toggleClass('modal-image__opened')
+
 })
